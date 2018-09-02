aac94c8e5b5ff81299bfba70ad8cb03f25505e6c
michele <michele.damico@gmail.com>
2018-09-02 22:45:51 +0200

    WIP: bug hunting... place holder for change lives

--- src/bin/space_invaders.rs
+++ src/bin/space_invaders.rs
@@ -101,7 +101,7 @@ fn main() {
 
     let mmu = SIMmu::new(rom.into(), vram.as_mut_ptr().into());
 
-    let io = Rc::new(IO::default());
+    let io = Rc::new(IO::default().change_lives(5));
 
     let mut cpu = Cpu::new(mmu, io.clone(), io.clone());
     let gpu = Gpu::new(W, H, vram.as_ptr());
@@ -144,7 +144,7 @@ fn main() {
             },
         }) as Box<WindowKey>
     ).collect();
-    let mut should_print_frame = true;
+    let mut should_print_frame = false;
     let mut print_frame = Box::new(ActiveKey {
         key: FlipFlopKey::from(DirectKey::from(Key::F)),
         action: {
