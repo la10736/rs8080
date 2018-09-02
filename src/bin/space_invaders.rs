1b653f2a2bd79135f87ff5078f1b9d9c86285fec
michele <michele.damico@gmail.com>
2018-09-02 15:14:08 +0200

    Ref: Extract cpu_run_till

--- src/bin/space_invaders.rs
+++ src/bin/space_invaders.rs
@@ -17,7 +17,8 @@ use std::rc::Rc;
 use std::io::Read;
 use si_memory::{SIMmu, ROM_SIZE, VRAM_SIZE};
 use si_io::IO;
-use rs8080::cpu::{Cpu, IrqCmd};
+use rs8080::cpu::Cpu as Cpu8080;
+use rs8080::cpu::{CpuError, IrqCmd};
 use gpu::Gpu;
 use std::time;
 use graphics::{WHITE, BLACK, Canvas, Rect};
@@ -27,6 +28,9 @@ use std::cell::RefCell;
 
 mod si_memory;
 mod si_io;
+
+type Cpu = Cpu8080<SIMmu, Rc<IO>, Rc<IO>>;
+
 
 #[derive(Default)]
 struct FlipFlop {
@@ -71,6 +75,117 @@ trait WindowKey {
 struct DirectKey {
     key: Key,
     last: KeyState,
+}
+
+fn main() {
+    simple_logger::init_with_level(log::Level::Warn).unwrap();
+
+    info!("SPACE INVADERS");
+
+    let mut rom = [0; ROM_SIZE];
+    let mut vram = [0; VRAM_SIZE];
+    load_rom(&mut rom, "resources");
+
+    let mmu = SIMmu::new(rom.into(), vram.as_mut_ptr().into());
+
+    let io = Rc::new(IO::default());
+
+    let mut cpu = Cpu::new(mmu, io.clone(), io.clone());
+    let gpu = Gpu::new(W, H, vram.as_ptr());
+
+    let w = W + 2 * W_MARGIN;
+    let h = H + 2 * H_MARGIN;
+
+    let mut window = Window::new("Space Invaders - ESC to exit",
+                                 w,
+                                 h,
+                                 WindowOptions::default()).unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+    let mut fb = vec![0; w * h];
+    let canvas = Canvas {
+        width: w,
+        height: h,
+        fg: WHITE,
+        bg: BLACK,
+    };
+    let (up, down) = Rect::from(&canvas).split_horizontal(h / 2);
+
+    let start = time::Instant::now();
+    let mut last_frame = start;
+    let mut frames: u64 = 1;
+    let mut upper = true;
+    let mut clocks: u64 = 0;
+    const CLOCK: u64 = 2_000_000;
+    const CLOCKS_PER_HALF_FRAME: u64 = CLOCK / 120;
+    const CLOCKS_PER_FRAME: u64 = CLOCKS_PER_HALF_FRAME * 2;
+
+    let paused = RefCell::new(false);
+    let mut pause = Box::new(ActiveKey {
+        key: FlipFlopKey::from(DirectKey::from(Key::P)),
+        action: |state| { *paused.borrow_mut() = state; },
+    });
+
+    let game_buttons = [
+        (Key::Key5, si_io::Ev::Coin),
+        (Key::Key1, si_io::Ev::P1Start),
+        (Key::Left, si_io::Ev::P1Left),
+        (Key::Right, si_io::Ev::P1Right),
+        (Key::Space, si_io::Ev::P1Shoot),
+    ];
+
+    let mut buttons: Vec<_> = game_buttons.into_iter().cloned()
+        .map(|(k, ev)| game_key(k, io.clone(), ev))
+        .collect();
+    buttons.push(pause);
+
+    loop {
+        if !window.is_open() || window.is_key_down(Key::Escape) {
+            debug!("Should terminate");
+            break;
+        }
+
+        buttons.iter_mut().for_each(|b| { b.update(&window); });
+
+        if !*paused.borrow() {
+            let expected_clocks = frames * CLOCKS_PER_FRAME;
+
+            // Up Frame
+            clocks = cpu_run_till(&mut cpu, clocks, expected_clocks)
+                .unwrap_or_else(|e| critical(&cpu, e));
+
+            gpu.fill_canvas(fb.as_mut(), &canvas, Some(up));
+            cpu.irq(IrqCmd::Irq1);
+
+            // Down Frame
+            clocks = cpu_run_till(&mut cpu, clocks, expected_clocks + CLOCKS_PER_HALF_FRAME)
+                .unwrap_or_else(|e| critical(&cpu, e));
+
+            gpu.fill_canvas(fb.as_mut(), &canvas, Some(down));
+            cpu.irq(IrqCmd::Irq2);
+
+            window.update_with_buffer(&fb);
+            frames += 1;
+
+            let limit = 25465654;
+
+            if clocks > limit - CLOCKS_PER_FRAME * 30 {
+//                println!("CLOCK = {}", clocks);
+            }
+
+//        if clocks >= limit + CLOCKS_PER_FRAME * 3 {
+//            dump(cpu);
+//            loop {
+//                std::thread::sleep(time::Duration::from_millis(1000));
+//            }
+//        }
+        } else {
+            std::thread::sleep(time::Duration::from_millis(100));
+            window.update();
+        }
+    }
+
+    info!("Game Done");
 }
 
 impl From<Key> for DirectKey {
@@ -153,129 +268,19 @@ fn game_key<'a, I: Deref<Target=IO> + 'a>(key: Key, io: I, ev: si_io::Ev) -> Box
     })
 }
 
-
-fn main() {
-    simple_logger::init_with_level(log::Level::Warn).unwrap();
-
-    info!("SPACE INVADERS");
-
-    let mut rom = [0; ROM_SIZE];
-    let mut vram = [0; VRAM_SIZE];
-    load_rom(&mut rom, "resources");
-
-    let mmu = SIMmu::new(rom.into(), vram.as_mut_ptr().into());
-
-    let io = Rc::new(IO::default());
-
-    let mut cpu = Cpu::new(mmu, io.clone(), io.clone());
-    let gpu = Gpu::new(W, H, vram.as_ptr());
-
-    let w = W + 2 * W_MARGIN;
-    let h = H + 2 * H_MARGIN;
-
-    let mut window = Window::new("Space Invaders - ESC to exit",
-                                 w,
-                                 h,
-                                 WindowOptions::default()).unwrap_or_else(|e| {
-        panic!("{}", e);
-    });
-    let mut fb = vec![0; w * h];
-    let canvas = Canvas {
-        width: w,
-        height: h,
-        fg: WHITE,
-        bg: BLACK,
-    };
-    let (up, down) = Rect::from(&canvas).split_horizontal(h / 2);
-
-    let start = time::Instant::now();
-    let mut last_frame = start;
-    let mut frames: u64 = 1;
-    let mut upper = true;
-    let mut clocks: u64 = 0;
-    const CLOCK: u64 = 2_000_000;
-    const CLOCKS_PER_HALF_FRAME: u64 = CLOCK / 120;
-    const CLOCKS_PER_FRAME: u64 = CLOCKS_PER_HALF_FRAME * 2;
-
-    let paused = RefCell::new(false);
-    let mut pause = Box::new(ActiveKey {
-        key: FlipFlopKey::from(DirectKey::from(Key::P)),
-        action: |state| {*paused.borrow_mut() = state;},
-    });
-
-    let game_buttons = [
-        (Key::Key5, si_io::Ev::Coin),
-        (Key::Key1, si_io::Ev::P1Start),
-        (Key::Left, si_io::Ev::P1Left),
-        (Key::Right, si_io::Ev::P1Right),
-        (Key::Space, si_io::Ev::P1Shoot),
-    ];
-
-    let mut buttons: Vec<_> = game_buttons.into_iter().cloned()
-        .map(|(k, ev)| game_key(k, io.clone(), ev))
-        .collect();
-    buttons.push(pause);
-
-    loop {
-        if !window.is_open() || window.is_key_down(Key::Escape) {
-            debug!("Should terminate");
-            break;
-        }
-
-        buttons.iter_mut().for_each(|b| { b.update(&window); });
-
-        if !*paused.borrow() {
-            let expected_clocks = (frames * CLOCKS_PER_FRAME) +
-                if upper {
-                    0
-                } else {
-                    CLOCKS_PER_HALF_FRAME
-                };
-
-            while clocks < expected_clocks {
-                match cpu.run() {
-                    Ok(c) => { clocks += c as u64; }
-                    Err(e) => {
-                        dump(&cpu);
-                        panic!("Received panic: {:?}", e);
-                    }
-                }
-            }
-            let limit = 25465654;
-
-            if clocks > limit - CLOCKS_PER_FRAME * 30 {
-//                println!("CLOCK = {}", clocks);
-            }
-
-//        if clocks >= limit + CLOCKS_PER_FRAME * 3 {
-//            dump(cpu);
-//            loop {
-//                std::thread::sleep(time::Duration::from_millis(1000));
-//            }
-//        }
-            // 1. Update Frame buffer
-            // 2. Fire IRQ
-            if upper {
-                gpu.fill_canvas(fb.as_mut(), &canvas, Some(up));
-                cpu.irq(IrqCmd::Irq1);
-            } else {
-                gpu.fill_canvas(fb.as_mut(), &canvas, Some(down));
-                cpu.irq(IrqCmd::Irq2);
-                window.update_with_buffer(&fb);
-                frames += 1;
-            };
-
-            upper = !upper;
-        } else {
-            std::thread::sleep(time::Duration::from_millis(100));
-            window.update();
-        }
+fn cpu_run_till(cpu: &mut Cpu, mut clocks: u64, expected_clocks: u64) -> Result<u64, CpuError> {
+    while clocks < expected_clocks {
+        clocks += cpu.run()? as u64;
     }
-
-    info!("Game Done");
+    Ok(clocks)
 }
 
-fn dump(cpu: &Cpu<SIMmu, Rc<IO>, Rc<IO>>) {
+fn critical(cpu: &Cpu, e: CpuError) -> ! {
+    dump(cpu);
+    panic!(e);
+}
+
+fn dump(cpu: &Cpu) {
     println!("State: \n{}", cpu.dump_state());
     println!("{}", cpu.dump_memory());
     println!("Stack: \n{}", cpu.dump_stack());
